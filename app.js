53affea280827837c0c27c869ad369d27d7089e6
aretmbarsukov <karasbobra@gmail.com>
2026-05-26 13:51:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const TWITCH_SECRET = process.env.TWITCH_SECRET;
 const streamers = [
   "steel","ravshann","renatko","steelaaga","ravshanbtw",
   "anarabdullaev","kerimch1k","renatkobmw","blslan",
-  "tadzheek","dedadam","vitollo_13","ereek","dankzlv","bratishkinoff"
+  "tadzheek","DedAdam","vitollo_13","ereek","dankzlv","bratishkinoff"
 ];
 
 // ---------------- Ініціалізація state ----------------
